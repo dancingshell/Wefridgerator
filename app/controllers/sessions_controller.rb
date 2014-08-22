049d8b47d3d4056bc234d410a43763e4d17a1e44
qbabin <q.babin@gmail.com>
2014-08-22 16:09:57 -0700

    Added User sessions and user new controller methods and current user helper method

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create											#returns user object to access email
+		@user = User.where(email: params[:user][:email]).first
+		if @user && @user.authenticate(params[:user][:password])
+			session[:user_id] = @user.id.to_s
+			redirect_to user_path(@user)
+		else
+			redirect_to new_session_path
+		end
+	end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user_id.to_s
+      redirect_to users_path
+    else
+      redirect_to root_path
+    end
   end
 
   def edit
