25e9379c89b90cb4b6499601cd04226591223cac
dancingshell <kaitlinjaffe@gmail.com>
2014-08-26 21:21:58 -0700

    added helper method for current_group

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :current_group
 
   def current_user
   	@current_user ||= User.where(:id => session[:user_id]).first 
   	#where will refer to an array, while find will refer to one id! This will find an id related to the user who logs in
   	#.first will refer to the id 
+  end
+
+  def current_groups
+    #Find all the user groups of the current_user
+    user_groups = UserGroup.where(user_id: current_user.id)
+    #Create an array of all the group_ids for those user_groups
+    groups = user_groups.map(&:group_id)
+    #Find the groups associated with those user_groups
+    Group.where(id: groups)
   end
 end
   
